38db0a27da632299bbfa01a9437831320da3109f
Canh8224 <lienkettaikhoan2312@gmail.com>
2025-02-13 13:15:03 +0700

    Thêm ResourceRepository.java và sửa đổi pom.xml và Resource.java

--- src/main/java/org/training/meetingroombooking/model/Resource.java
+++ src/main/java/org/training/meetingroombooking/model/Resource.java
@@ -1,5 +1,35 @@
 package org.training.meetingroombooking.model;
 
 public class Resource {
-
+private long resourceId;
+private String resourceName;
+private String email;
+private String phone;
+private String StepProcess;
+private String Create;
+private String Assignee;
+private String action;
+public  Resource() {}
+    public  Resource(long resourceId, String resourceName, String email, String phone, String stepProcess, String create, String assignee) {
+    this.resourceId = resourceId;
+    this.resourceName = resourceName;
+    this.email = email;
+    this.phone = phone;
+    this.StepProcess = stepProcess;
+    this.Create = create;
+    this.Assignee = assignee;
+    this.action = action;
+    }
+    public long getResourceId() {
+    return resourceId;
+    }
+    public void setResourceId(long resourceId) {
+    this.resourceId = resourceId;
+    }
+    public String getResourceName() {
+    return resourceName;
+    }
+    public void setResourceName(String resourceName) {
+    }
 }
+
