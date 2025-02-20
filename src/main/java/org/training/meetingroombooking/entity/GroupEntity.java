b08b8d7f0b2eadae953e84b797ca68fd70684be6
Vu. Phung Van- CMC Global DU3.11 <pvvu@cmcglobal.vn>
2025-02-20 08:39:52 +0700

    update 20/2

--- src/main/java/org/training/meetingroombooking/entity/GroupEntity.java
+++ src/main/java/org/training/meetingroombooking/entity/GroupEntity.java
@@ -1,6 +1,11 @@
 package org.training.meetingroombooking.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.time.LocalDate;
 
 @Entity
--- src/main/java/org/training/meetingroombooking/mapper/UserMapper.java
+++ src/main/java/org/training/meetingroombooking/mapper/UserMapper.java
@@ -1,10 +1,11 @@
 package org.training.meetingroombooking.mapper;
 
-import org.training.meetingroombooking.dto.UserDTO;
+import org.training.meetingroombooking.dto.Request.UserRequest;
+import org.training.meetingroombooking.dto.Response.UserResponse;
 import org.training.meetingroombooking.entity.User;
 
 public interface UserMapper {
-    User toEntity(UserDTO userDTO);
-    UserDTO toDTO(User user);
-    void updateEntity(User user, UserDTO userDTO);
+    User toEntity(UserRequest request);
+    UserResponse toDTO(User user);
+    void updateEntity(User user, UserRequest request);
 }
