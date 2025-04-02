316e1d93e7e3febd77773660880e1ff48f3f903f
phungvanvu <phungvanvu0@gmail.com>
2025-04-02 23:37:48 +0700

    2/4

--- src/main/java/org/training/meetingroombooking/controller/FileController.java
+++ src/main/java/org/training/meetingroombooking/controller/FileController.java
@@ -1,11 +1,10 @@
 package org.training.meetingroombooking.controller;
 
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import java.net.MalformedURLException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,15 +16,13 @@ public class FileController {
   private final Path ***REMOVED*** = Paths.get("uploads");
 
   @GetMapping("/rooms/{filename:.+}")
-  public ResponseEntity<Resource> getFile(
-      @PathVariable String filename) throws MalformedURLException {
+  public ResponseEntity<Resource> getFile(@PathVariable String filename) throws MalformedURLException {
     Path file = ***REMOVED***.resolve("rooms").resolve(filename);
     Resource resource = new UrlResource(file.toUri());
     if (resource.exists() || resource.isReadable()) {
       return ResponseEntity.ok()
-          .header(HttpHeaders.CONTENT_DISPOSITION,
-              "inline; filename=\"" + filename + "\"")
-          .body(resource);
+              .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + filename + "\"")
+              .body(resource);
     } else {
       throw new RuntimeException("File not found: " + filename);
     }
