0c066f023cc911623931ce10eed1d0b04352a9c1
Vu. Phung Van- CMC Global DU3.11 <pvvu@cmcglobal.vn>
2025-04-02 11:44:30 +0700

    2/4

--- src/main/java/org/training/meetingroombooking/repository/UserRepository.java
+++ src/main/java/org/training/meetingroombooking/repository/UserRepository.java
@@ -1,13 +1,14 @@
 package org.training.meetingroombooking.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import org.training.meetingroombooking.entity.models.User;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
     Optional<User> findByUserName(String userName);
     boolean existsByUserName(String userName);
 }
