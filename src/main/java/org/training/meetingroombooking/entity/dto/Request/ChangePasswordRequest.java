19e2dd05d5d7ce3bb7704b73e159b6dae7d6824c
Vu. Phung Van- CMC Global DU3.11 <pvvu@cmcglobal.vn>
2025-04-15 11:20:14 +0700

    15/5

--- src/main/java/org/training/meetingroombooking/entity/dto/Request/ChangePasswordRequest.java
+++ src/main/java/org/training/meetingroombooking/entity/dto/Request/ChangePasswordRequest.java
@@ -1,6 +1,7 @@
 package org.training.meetingroombooking.entity.dto.Request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,9 +12,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class ChangePasswordRequest {
+    @Pattern(regexp = "^[^\\s]+$", message = "Username and password should not contain any spaces")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+        message = "Password must be at least 8 characters, contain at least one uppercase letter, one lowercase letter, one number, and one special character")
     @NotBlank(message = "Old password must not be blank")
     private String oldPassword;
 
+    @Pattern(regexp = "^[^\\s]+$", message = "Username and password should not contain any spaces")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+        message = "Password must be at least 8 characters, contain at least one uppercase letter, one lowercase letter, one number, and one special character")
     @NotBlank(message = "New password must not be blank")
     private String newPassword;
 }
